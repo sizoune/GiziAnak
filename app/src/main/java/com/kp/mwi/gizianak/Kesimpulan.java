2dbd7832a5380d8b3570846959ceb4db3f7318f0
mwildani <muhammadwildaniskandar@gmail.com>
2017-06-09 05:46:41 +0700

    fix bug for Xaxis value

--- app/src/main/java/com/kp/mwi/gizianak/Kesimpulan.java
+++ app/src/main/java/com/kp/mwi/gizianak/Kesimpulan.java
@@ -27,6 +27,7 @@ import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -220,26 +221,6 @@ public class Kesimpulan extends AppCompatActivity implements View.OnClickListene
     }
 
     private void diatas5tahun(float sumbx, float sumby) {
-        String[] valuesX = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"
-                , "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"
-                , "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36"
-                , "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"
-                , "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60"
-                , "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72"
-                , "73", "74", "75", "76", "77", "78", "79", "70", "71", "72", "73", "74"
-                , "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86"
-                , "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98"
-                , "99", "100", "101", "102", "103", "104", "105", "106", "107", "108", "109", "110"
-                , "111", "112", "113", "114", "115", "116", "117", "118", "119", "120", "121", "122"
-                , "123", "124", "125", "126", "127", "128", "129", "130", "131", "132", "133", "134"
-                , "135", "136", "137", "138", "139", "140", "141", "142", "143", "144", "145", "146"
-                , "147", "148", "149", "150", "151", "152", "153", "154", "155", "156", "157", "158"
-                , "159", "160", "161", "162", "163", "164", "165", "166", "167", "168", "169", "170"
-                , "170", "171", "172", "173", "174", "175", "176", "177", "178", "179", "180"
-                , "181", "182", "183", "184", "185", "186", "187", "188", "189", "190", "191", "192"
-                , "193", "194", "195", "196", "197", "198", "199", "200", "201", "202", "203", "204"
-                , "205", "206", "207", "208", "209", "210", "211", "212", "213", "214", "215", "216"};
-
         ArrayList<Entry> garisG = new ArrayList<>();
         ArrayList<Entry> garisF = new ArrayList<>();
         ArrayList<Entry> garisE = new ArrayList<>();
@@ -252,15 +233,6 @@ public class Kesimpulan extends AppCompatActivity implements View.OnClickListene
         dataUser.add(new Entry(sumbx, sumby));
 
         if (dk.getStatus().equals("BeratUmur")) {
-            String[] valuesKg = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"
-                    , "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"
-                    , "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36"
-                    , "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"
-                    , "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60"
-                    , "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72"
-                    , "73", "74", "75", "76", "77", "78", "79", "70", "71", "72", "73", "74"
-                    , "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86"
-                    , "87", "88", "89", "90", "91", "92", "93", "94", "95", "96"};
 
             garisG.add(new Entry(66, 16));
             garisF.add(new Entry(66, 17));
@@ -521,15 +493,33 @@ public class Kesimpulan extends AppCompatActivity implements View.OnClickListene
             dataSets.add(datasetA);
             dataSets.add(siUser);
 
-            chart.getXAxis().setValueFormatter(new MyXAxisValueFormatter(valuesX));
-            chart.getAxisLeft().setValueFormatter(new MyYAxisValueFormatter(valuesKg));
-            chart.getAxisRight().setValueFormatter(new MyYAxisValueFormatter(valuesKg));
+            chart.getXAxis().resetAxisMinimum();
+            chart.getXAxis().resetAxisMaximum();
+            chart.getXAxis().setAxisMinimum(66);
+            chart.getXAxis().setAxisMaximum(216);
+
+            chart.getAxisLeft().resetAxisMaximum();
+            chart.getAxisLeft().resetAxisMinimum();
+            chart.getAxisRight().resetAxisMaximum();
+            chart.getAxisRight().resetAxisMinimum();
+            chart.getAxisLeft().setAxisMaximum(82);
+            chart.getAxisLeft().setAxisMinimum(16);
+            chart.getAxisRight().setAxisMaximum(82);
+            chart.getAxisRight().setAxisMinimum(16);
+
+            chart.getXAxis().getValueFormatter();
+
+//            chart.getXAxis().setValueFormatter(new MyXAxisValueFormatter(valuesX));
+//            chart.getAxisLeft().setValueFormatter(new MyYAxisValueFormatter(valuesKg));
+//            chart.getAxisRight().setValueFormatter(new MyYAxisValueFormatter(valuesKg));
 
             chart.setData(new LineData(dataSets));
 
+            chart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+
             Description description = new Description();
             chart.getLegend().setEnabled(false);
-            description.setText("");
+            description.setText("Sumbu x : umur dalam bulan, sumbu y : berat dalam Kg");
             chart.setDescription(description);
             chart.invalidate();
 
@@ -538,24 +528,6 @@ public class Kesimpulan extends AppCompatActivity implements View.OnClickListene
                     " the National Center for Chronic Disease Prevention and Health Promotion (2000)");
             sumber.setVisibility(View.VISIBLE);
         } else if (dk.getStatus().equals("TinggiUmur")) {
-            String[] valuesCm = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"
-                    , "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"
-                    , "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36"
-                    , "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"
-                    , "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60"
-                    , "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72"
-                    , "73", "74", "75", "76", "77", "78", "79", "70", "71", "72", "73", "74"
-                    , "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86"
-                    , "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98"
-                    , "99", "100", "101", "102", "103", "104", "105", "106", "107", "108", "109", "110"
-                    , "111", "112", "113", "114", "115", "116", "117", "118", "119", "120", "121", "122"
-                    , "123", "124", "125", "126", "127", "128", "129", "130", "131", "132", "133", "134"
-                    , "135", "136", "137", "138", "139", "140", "141", "142", "143", "144", "145", "146"
-                    , "147", "148", "149", "150", "151", "152", "153", "154", "155", "156", "157", "158"
-                    , "159", "160", "161", "162", "163", "164", "165", "166", "167", "168", "169", "170"
-                    , "170", "171", "172", "173", "174", "175", "176", "177", "178", "179", "180"
-                    , "181", "182", "183", "184", "185", "186", "187", "188", "189", "190", "191", "192"
-                    , "193", "194", "195"};
 
             garisG.add(new Entry(66, 104));
             garisF.add(new Entry(66, 106));
@@ -816,15 +788,31 @@ public class Kesimpulan extends AppCompatActivity implements View.OnClickListene
             dataSets.add(datasetA);
             dataSets.add(siUser);
 
-            chart.getXAxis().setValueFormatter(new MyXAxisValueFormatter(valuesX));
-            chart.getAxisLeft().setValueFormatter(new MyCMYAxisValueFormatter(valuesCm));
-            chart.getAxisRight().setValueFormatter(new MyCMYAxisValueFormatter(valuesCm));
+            chart.getXAxis().resetAxisMinimum();
+            chart.getXAxis().resetAxisMaximum();
+            chart.getXAxis().setAxisMinimum(66);
+            chart.getXAxis().setAxisMaximum(216);
+
+            chart.getAxisLeft().resetAxisMaximum();
+            chart.getAxisLeft().resetAxisMinimum();
+            chart.getAxisRight().resetAxisMaximum();
+            chart.getAxisRight().resetAxisMinimum();
+            chart.getAxisLeft().setAxisMaximum(188);
+            chart.getAxisLeft().setAxisMinimum(104);
+            chart.getAxisRight().setAxisMaximum(188);
+            chart.getAxisRight().setAxisMinimum(104);
+
+//            chart.getXAxis().setValueFormatter(new MyXAxisValueFormatter(valuesX));
+//            chart.getAxisLeft().setValueFormatter(new MyCMYAxisValueFormatter(valuesCm));
+//            chart.getAxisRight().setValueFormatter(new MyCMYAxisValueFormatter(valuesCm));
 
             chart.setData(new LineData(dataSets));
 
+            chart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+
             Description description = new Description();
             chart.getLegend().setEnabled(false);
-            description.setText("");
+            description.setText("Sumbu x : umur dalam bulan, sumbu y : tinggi dalam cm");
             chart.setDescription(description);
             chart.invalidate();
 
@@ -1275,6 +1263,8 @@ public class Kesimpulan extends AppCompatActivity implements View.OnClickListene
             dataSets.add(datasetB);
             dataSets.add(datasetA);
             dataSets.add(siUser);
+
+            chart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
 
             chart.getXAxis().setValueFormatter(new MyXAxisValueFormatter(values));
             chart.getAxisLeft().setValueFormatter(new MyYAxisValueFormatter(valuesKg));
@@ -2132,6 +2122,8 @@ public class Kesimpulan extends AppCompatActivity implements View.OnClickListene
             dataSets.add(datasetA1);
             dataSets.add(siUser);
 
+            chart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+
             chart.getXAxis().setValueFormatter(new MyXAxisValueFormatter(values));
             chart.getAxisLeft().setValueFormatter(new MyCMYAxisValueFormatter(valuesCm));
             chart.getAxisRight().setValueFormatter(new MyCMYAxisValueFormatter(valuesCm));
@@ -2620,6 +2612,8 @@ public class Kesimpulan extends AppCompatActivity implements View.OnClickListene
             dataSets.add(datasetB1);
             dataSets.add(datasetA1);
             dataSets.add(siUser);
+
+            chart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
 
             chart.getXAxis().setValueFormatter(new MyXAxisValueFormatter(values));
             chart.getAxisLeft().setValueFormatter(new MyBMIYAxisValueFormatter(valuesBMI));
